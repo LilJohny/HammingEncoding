e302cd88983995062160d90cee8ed2bcf9fc216f
Denis Ivanenko <42393830+LilJohny@users.noreply.github.com>
2019-06-13 23:27:39 +0300

    Update main.py
    Fixed case sensetive

--- Encoder/main.py
+++ Encoder/main.py
@@ -4,6 +4,7 @@ from encoding import Encoder
 def main():
     encoder = Encoder()
     message = input("Type message to encode: ")
+    message = message.lower()
     encoded_message, encoding_table = encoder.encode_message(message)
     print("Encoding table :")
     print(encoding_table)
