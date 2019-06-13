7d3033ed4e95ba263ea852a9d913d58cda438e4f
captainvlad <zadorozhnyy@ucu.edu.ua>
2019-06-13 11:16:23 +0300

    'F'

--- Encoder/encoding.py
+++ Encoder/encoding.py
@@ -1,125 +1,165 @@
 from tree import Tree
 
+class Encoding:
+    '''
+    This is class for encoding needed message
+    '''
+    def __init__(self, message):
+        '''
+        obj, str -> None
+        This method initializes an object and performs all needed actions
+        '''
+        self.phrase = message
+        self.get_possibilities()
+        print(self.possibilities)
+        self.encode()
+        rez = list(filter(lambda x: x != -1, self.decode_tree(self.tree)))
+        rez = dict(rez)
 
-def get_possibilities(phrase):
-    symbols = list(set(list(phrase)))
-    for i in range(len(symbols)):
-        symbols[i] = symbols[i], phrase.count(symbols[i]) / len(phrase)
-    return sorted(symbols, key=lambda x: x[1], reverse=True)
+        print(self.phrase.translate(str.maketrans(rez)))
+        print(rez)
 
-
-def sort_key(object_):
-    if isinstance(object_, Tree):
-        return object_.data[-1]
-    else:
-        return object_[-1]
-
-
-def sorted_possibilities(possibilities):
-    possibilities = sorted(possibilities, key=sort_key, reverse=True)
-    result = []
-    possibilities_vals = set(map(get_possibility, possibilities))
-    for possibility_val in sorted(possibilities_vals, reverse=True):
-        possibility_tuples = list(
-            filter(lambda x: isinstance(x, tuple) and x[-1] == possibility_val,
-                   possibilities))
-        possibility_trees = list(
-            filter(
-                lambda x: isinstance(x, Tree) and x.data[-1] ==
-                possibility_val, possibilities))
-        possibility_tuples = sorted(possibility_tuples,
-                                    key=lambda x: x[0],
-                                    reverse=True)
-        result.extend(possibility_tuples)
-        result.extend(possibility_trees)
-    return result
+    def get_possibilities(self):
+        '''
+        obj -> None
+        This method counts possibilities of every letter in the message
+        '''
+        symbols = list(set(list(self.phrase)))
+        for i in range(len(symbols)):
+            symbols[i] = symbols[i], self.phrase.count(symbols[i]) / len(self.phrase)
+        self.possibilities = sorted(symbols, key=lambda x: x[1], reverse=True)
 
 
-def calculate_sum_pos(object_1, object_2):
-    sum_pos = 0
-    if isinstance(object_1, tuple):
-        sum_pos += object_1[-1]
-    elif isinstance(object_1, Tree):
-        sum_pos += object_1.data[-1]
-    if isinstance(object_2, tuple):
-        sum_pos += object_2[-1]
-    elif isinstance(object_2, Tree):
-        sum_pos += object_2.data[-1]
-    return sum_pos
-
-
-def get_possibility(encode_sym):
-    if isinstance(encode_sym, tuple):
-        return encode_sym[-1]
-    elif isinstance(encode_sym, Tree):
-        return encode_sym.data[-1]
-
-
-def encode(possibilities):
-    possibilities = sorted_possibilities(possibilities)
-    while len(possibilities) != 1:
-        f_possibility = possibilities[-1]
-        s_possibility = possibilities[-2]
-        f_possibility = Tree(
-            ('', calculate_sum_pos(f_possibility, s_possibility)))
-        if get_possibility(
-                possibilities[-1]) <= get_possibility(s_possibility):
-            f_possibility.left = possibilities[-1]
-            f_possibility.right = s_possibility
+    def sort_key(self, object_):
+        '''
+        This method sorts letters according to possibilities
+        '''
+        if isinstance(object_, Tree):
+            return object_.data[-1]
         else:
-            f_possibility.left = s_possibility
-            f_possibility.right = possibilities[-1]
-        del possibilities[-1]
-        del possibilities[-1]
-        possibilities.append(f_possibility)
-        #possibilities = sorted(possibilities, key=sort_key, reverse=True)
-        possibilities = sorted_possibilities(possibilities)
-    return possibilities[-1]
+            return object_[-1]
 
 
-def decode_tree(tree):
-    def _decode_child(code_tree, code):
-        if isinstance(code_tree.left, tuple):
-            encoded = (code_tree.left[0], code + '0')
-            code_tree.left = None
-            if code_tree.left is None and code_tree.right is None:
-                code_tree = None
-            return encoded
-        if isinstance(code_tree.right, tuple):
-            encoded = (code_tree.right[0], code + '1')
-            code_tree.right = None
-            if code_tree.left is None and code_tree.right is None:
-                code_tree = None
-            return encoded
-        if isinstance(code_tree.left, Tree):
-            if code_tree.left.left is None and code_tree.left.right is None:
+    def sorted_possibilities(self, possibilities):
+        '''
+        obj, dict -> list
+        This method sorts possibilites
+        '''
+        possibilities = sorted(possibilities, key=self.sort_key, reverse=True)
+        result = []
+        possibilities_vals = set(map(self.get_possibility, possibilities))
+        for possibility_val in sorted(possibilities_vals, reverse=True):
+            possibility_tuples = list(
+                filter(lambda x: isinstance(x, tuple) and x[-1] == possibility_val,
+                       possibilities))
+            possibility_trees = list(
+                filter(
+                    lambda x: isinstance(x, Tree) and x.data[-1] ==
+                    possibility_val, possibilities))
+            possibility_tuples = sorted(possibility_tuples,
+                                        key=lambda x: x[0],
+                                        reverse=True)
+            result.extend(possibility_tuples)
+            result.extend(possibility_trees)
+        return result
+
+
+    def calculate_sum_pos(self, object_1, object_2):
+        '''
+        obj, int, int -> int
+        This method calculates code for every letter
+        '''
+        sum_pos = 0
+        if isinstance(object_1, tuple):
+            sum_pos += object_1[-1]
+        elif isinstance(object_1, Tree):
+            sum_pos += object_1.data[-1]
+        if isinstance(object_2, tuple):
+            sum_pos += object_2[-1]
+        elif isinstance(object_2, Tree):
+            sum_pos += object_2.data[-1]
+        return sum_pos
+
+
+    def get_possibility(self, encode_sym):
+        '''
+        obj, str -> int
+        This method returns possibility for symbol
+        '''
+        if isinstance(encode_sym, tuple):
+            return encode_sym[-1]
+        elif isinstance(encode_sym, Tree):
+            return encode_sym.data[-1]
+
+
+    def encode(self):
+        '''
+        obj -> None
+        This method totally encodes given message
+        '''
+        possibilities = self.sorted_possibilities(self.possibilities)
+        while len(possibilities) != 1:
+            f_possibility = possibilities[-1]
+            s_possibility = possibilities[-2]
+            f_possibility = Tree(
+                ('', self.calculate_sum_pos(f_possibility, s_possibility)))
+            if self.get_possibility(
+                    possibilities[-1]) <= self.get_possibility(s_possibility):
+                f_possibility.left = possibilities[-1]
+                f_possibility.right = s_possibility
+            else:
+                f_possibility.left = s_possibility
+                f_possibility.right = possibilities[-1]
+            del possibilities[-1]
+            del possibilities[-1]
+            possibilities.append(f_possibility)
+            possibilities = self.sorted_possibilities(possibilities)
+        self.tree = possibilities[-1]
+
+
+    def decode_tree(self, tree):
+        '''
+        obj, obj -> list
+        This method decodes tree made from the message
+        '''
+        def _decode_child(code_tree, code):
+            if isinstance(code_tree.left, tuple):
+                encoded = (code_tree.left[0], code + '0')
                 code_tree.left = None
-            else:
-                return _decode_child(code_tree.left, code + '0')
-        if isinstance(code_tree.right, Tree):
-            if code_tree.right.left is None and code_tree.right.right is None:
+                if code_tree.left is None and code_tree.right is None:
+                    code_tree = None
+                return encoded
+            if isinstance(code_tree.right, tuple):
+                encoded = (code_tree.right[0], code + '1')
                 code_tree.right = None
-                return -1
-            else:
-                return _decode_child(code_tree.right, code + '1')
+                if code_tree.left is None and code_tree.right is None:
+                    code_tree = None
+                return encoded
+            if isinstance(code_tree.left, Tree):
+                if code_tree.left.left is None and code_tree.left.right is None:
+                    code_tree.left = None
+                else:
+                    return _decode_child(code_tree.left, code + '0')
+            if isinstance(code_tree.right, Tree):
+                if code_tree.right.left is None and code_tree.right.right is None:
+                    code_tree.right = None
+                    return -1
+                else:
+                    return _decode_child(code_tree.right, code + '1')
 
-    result = []
-    encoded_sym = 0
-    while encoded_sym is not None:
-        encoded_sym = _decode_child(tree, "")
-        if encoded_sym is not None:
-            result.append(encoded_sym)
-    return result
+        result = []
+        encoded_sym = 0
+        while encoded_sym is not None:
+            encoded_sym = _decode_child(tree, "")
+            if encoded_sym is not None:
+                result.append(encoded_sym)
+        return result
 
+print('This is example of encoding word "hello" with Huffman code: ')
+p = Encoding("hello")
+while True:
+    command = input('Enter messsage to encode or "1" to finish working: ')
+    if command == '1':
+        break
+    p = Encoding(command.lower())
 
-phrase = "hello"
-p = get_possibilities(phrase)
-print(p)
-t = encode(p)
-rez = decode_tree(t)
-rez = list(filter(lambda x: x != -1, rez))
-rez = dict(rez)
-
-print(phrase.translate(str.maketrans(rez)))
-print(rez)
-k = 0
--- Encoder/tree.py
+++ Encoder/tree.py
@@ -1,28 +1,59 @@
 class Tree:
+    '''
+    This is realization of a tree for exact project
+    '''
     def __init__(self, data):
+        '''
+        obj, str -> None
+        This method initializes an object
+        '''
         self.data = data
         self._left = None
         self._right = None
 
     def set_left(self, value):
+        '''
+        obj, str -> None
+        This puts needed value on the left
+        '''
         self._left = value
 
     def get_left(self):
+        '''
+        obj, str -> None
+        This returns needed value on the left
+        '''
         return self._left
 
     left = property(fset=set_left, fget=get_left)
 
     def set_right(self, value):
+        '''
+        obj, str -> None
+        This puts needed value on the right
+        '''
         self._right = value
 
     def get_right(self):
+        '''
+        obj, str -> None
+        This returns needed value on the right
+        '''
         return self._right
 
     def __repr__(self):
+        '''
+        obj -> str
+        This method represents object
+        '''
         return f"{self.data}"
 
 
     right = property(fset=set_right, fget=get_right)
 
     def is_empty(self):
+        '''
+        obj -> bool
+        This method checks whether tree is empty
+        '''
         return self._left is None and self._right is None
